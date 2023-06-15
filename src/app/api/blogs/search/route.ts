759ece71a1af9543236f4bda45172922f83f77f2
zonaetmunna <zonaetmonna@gmail.com>
2023-06-15 15:33:07 +0600

    add

--- src/app/api/blogs/search/route.ts
+++ src/app/api/blogs/search/route.ts
@@ -1,12 +1,1 @@
-/* import { NextRequest, NextResponse } from "next/server";
-import blogs from "../data.json";
 
-export async function handleGet(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const query = searchParams.get("query");
-  const filterBlog = blogs.filter((blog) => {
-    return blog.title.toLowerCase().includes(query?.toLowerCase());
-  });
-  return new NextResponse.json(filterBlog);
-}
- */
